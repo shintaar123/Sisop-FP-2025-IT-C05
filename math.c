209ebdfc2eb4180ac6d5cd8e382af7cb55da501f
shintaar123 <shintaar051005@gmail.com>
2025-06-20 21:08:24 +0700

    second commit

--- math.c
+++ math.c
@@ -4,14 +4,13 @@
 #include <sys/wait.h>
 
 int main() {
-    int fd[2]; // 0 untuk read, 1 untuk write
+    int fd[2]; 
     pid_t pid;
     int n;
 
     printf("Masukkan angka maksimal: ");
     scanf("%d", &n);
 
-    // Buat pipe
     if (pipe(fd) == -1) {
         perror("Pipe gagal");
         return 1;
@@ -25,28 +24,25 @@ int main() {
     }
 
     if (pid > 0) {
-        // Parent process
-        close(fd[0]); // Tutup read end
+        close(fd[0]); 
 
         int sum = 0;
         for (int i = 1; i <= n; i++) {
             sum += i;
         }
 
-        // Kirim hasil ke child
         write(fd[1], &sum, sizeof(sum));
-        close(fd[1]); // Tutup write end
-
-        wait(NULL); // Tunggu child selesai
+        close(fd[1]); 
+        
+        wait(NULL); 
     } else {
-        // Child process
-        close(fd[1]); // Tutup write end
-
+        close(fd[1]); 
+        
         int result;
         read(fd[0], &result, sizeof(result));
         printf("Child: Jumlah dari 1 sampai %d adalah %d\n", n, result);
 
-        close(fd[0]); // Tutup read end
+        close(fd[0]);
     }
 
     return 0;
